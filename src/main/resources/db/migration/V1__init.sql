60b65fc3ab20b2cb48d0933dbfae77ecda78f38e
jeka <eu-1982@yandex.ru>
2022-04-12 09:27:57 +0700

    Lesson3

--- src/main/resources/db/migration/V1__init.sql
+++ src/main/resources/db/migration/V1__init.sql
@@ -53,7 +53,8 @@ values ('ROLE_USER'),
        ('ROLE_ADMIN');
 
 insert into users (username, password, email)
-values ('bob', '$2a$04$Fx/SX9.BAvtPlMyIIqqFx.hLY2Xp8nnhpzvEEVINvVpwIPbA3v/.i', 'bob_johnson@gmail.com'),
+values ('user','$2a$12$niT9crQOsMU45R8f62hw7uoo15Dzi464B5KxT5TaPNmf8R4qqVCcq','user@user.ru'),
+       ('bob', '$2a$04$Fx/SX9.BAvtPlMyIIqqFx.hLY2Xp8nnhpzvEEVINvVpwIPbA3v/.i', 'bob_johnson@gmail.com'),
        ('john', '$2a$04$Fx/SX9.BAvtPlMyIIqqFx.hLY2Xp8nnhpzvEEVINvVpwIPbA3v/.i', 'john_johnson@gmail.com');
 
 insert into users_roles (user_id, role_id)
