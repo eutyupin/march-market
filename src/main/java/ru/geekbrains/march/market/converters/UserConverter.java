26afbf4ab71f1a52ee352eaa0cb596d9dc3809a7
jeka <eu-1982@yandex.ru>
2022-04-12 14:21:37 +0700

    HW3

--- src/main/java/ru/geekbrains/march/market/converters/UserConverter.java
+++ src/main/java/ru/geekbrains/march/market/converters/UserConverter.java
@@ -3,10 +3,12 @@ package ru.geekbrains.march.market.converters;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
 import ru.geekbrains.march.market.dtos.UserDto;
 import ru.geekbrains.march.market.entities.User;
 import ru.geekbrains.march.market.services.UserService;
 
+@Component
 @RequiredArgsConstructor
 public class UserConverter {
     private final UserService userService;
