2d28efc839a72b802b4ec2ddfd1cc77fde36538c
jeka <eu-1982@yandex.ru>
2022-04-12 15:07:19 +0700

    HW3

--- src/main/java/ru/geekbrains/march/market/services/UserService.java
+++ src/main/java/ru/geekbrains/march/market/services/UserService.java
@@ -36,6 +36,4 @@ public class UserService implements UserDetailsService {
     private Collection<? extends GrantedAuthority> mapRolesToAuthorities(Collection<Role> roles) {
         return roles.stream().map(role -> new SimpleGrantedAuthority(role.getName())).collect(Collectors.toList());
     }
-
-
 }
